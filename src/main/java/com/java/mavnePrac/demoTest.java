2f8ace0da61913c589ea8cac9791a3b8afc352b9
bhargavivarala <bhargavivarala11@gmail.com>
2020-05-20 19:11:40 +0530

    first commit

---
+++ src/main/java/com/java/mavnePrac/demoTest.java
@@ -0,0 +1,25 @@
+package com.java.mavnePrac;
+
+import org.testng.annotations.Test;
+
+public class demoTest {
+
+		@Test
+		public void login() {
+			System.out.println("Test");
+		}
+
+		@Test
+		public void Bookflight() { 
+			System.out.println("Test priority1");
+		}
+
+		@Test
+		public void search() {
+			System.out.println("Test priority2");
+		}
+
+	
+	}
+
+
---
+++ src/test/java/com/java/mavnePrac/applicationTest.java
@@ -0,0 +1,30 @@
+package com.java.mavnePrac;
+
+import org.testng.annotations.Test;
+
+public class applicationTest {
+	@Test
+	public void shoppingdemart() {
+		System.out.println("demart store");
+	}
+
+	@Test
+	public void shoppingmore() {
+		System.out.println("more store");
+	}
+
+	@Test
+	public void shoppingbigbazar() {
+		System.out.println("bigbazar");
+	}
+
+	@Test
+	public void onlineamazon() {
+		System.out.println("amazon");
+	}
+
+	@Test
+	public void onlineflipkart() {
+		System.out.println("flipkart");
+	}
+}
